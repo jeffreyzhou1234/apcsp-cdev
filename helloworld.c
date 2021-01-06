faffc8681e9580920aa4b57eb64b2c68bc669d88
Jeffrey Zhou <zhouz2090@my.sduhsd.net>
2021-01-06 07:21:33 +0000

    C Activity 1

--- helloworld.c
+++ helloworld.c
@@ -1,7 +1,10 @@
-#includ <stdio.h>
+#include <stdio.h>
 
 int main()
 {
-    printf("Hello World!\n");
+    printf("Hellow World!\n");
+    printf("CCA is the best\n");
+    printf("APCSP rocks\n");
     return 0;
 }
+
