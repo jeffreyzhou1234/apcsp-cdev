c868ff560013eaff5c8a3d412647d13a66b4bbcf
Jeffrey Zhou <zhouz2090@my.sduhsd.net>
2021-01-23 01:11:42 +0000

    C Final Project

--- proj/student.c
+++ proj/student.c
@@ -7,7 +7,7 @@
 #include <string.h>
 
 #include "student.h"
-#include "encrypt.h"
+#include "encrypt/encrypt.h"
 
 
 const char* STUFILE = "studentdata.txt";
@@ -18,7 +18,6 @@ int numStudents = 0;
 // holds the student pointer array
 Student* students[100] = {0};
 
-
 void createStudent(char* fname, char* lname, int age, int id)
 {
   // createStudent dynamically creates a Student on the heap and adds that
@@ -26,10 +25,10 @@ void createStudent(char* fname, char* lname, int age, int id)
   //  - the firstName and lastName strings should be dynamically created
   //    based on the size of the fname and lname args
   Student* st = (Student*)malloc(sizeof(Student));
-  st->fname = (char*)malloc((strlen(fname)+1))*sizeof(char));
-  st->lname = (char*)malloc((strlen(lname)+1))*sizeof(char));
-  strcpy(st->firstname, fname);
-  strcpy(st->lastname, lname);
+  st->firstName = (char*)malloc(strlen(fname)+1);
+  st->lastName = (char*)malloc(strlen(lname)+1);
+  strcpy(st->firstName, fname);
+  strcpy(st->lastName, lname);
   st->age = age;
   st->id = id;
   students[numStudents] = st;
@@ -40,9 +39,9 @@ void createStudent(char* fname, char* lname, int age, int id)
 void deleteStudent(Student* student)
 {
   // free the memory associated with a student including the strings
-  free(st->fname);
-  free(st->lname);
-  free(st);
+  free(student->firstName);
+  free(student->lastName);
+  free(student);
 }
 
 
@@ -51,8 +50,8 @@ void deleteStudents()
   // iterate over the students array deleting every student and setting te pointer
   // values to 0 and adjusting the numStudents to 0
   for(int i = 0;i < numStudents;i++){
-    deleteStudent(student[i]);
-    student[i]=0;
+    deleteStudent(students[i]);
+    students[i]=0;
   }
   numStudents = 0;
 }
@@ -69,18 +68,21 @@ void saveStudents(int key)
   // the best way to do this is to convert the student data to a string using sprintf and then
   // (optionally) encrypt the whole string before writing it to disk (see cdemo/fileio3.c)
   FILE* fp;
-  fp = fopen("studentdata.txt", "w");
-  char buff[256];
+  fp = fopen(STUFILE, "w");
   if(fp){
+    char buff[256];
     for(int i = 0; i < numStudents;i++){
       Student* st = students[i];
-      sprintf(buff, "%s %s %d %ld", st->fname, st->lname, st->age, st->id);
+      sprintf(buff, "%s %s %d %ld", st->firstName, st->lastName, st->age, st->id);
       if(key != 0){
         caesarEncrypt(buff, key);
       }
-      fprintf(fp, "%s", buff);
+      printf("Saving %s\n", buff);
+      fprintf(fp, "%s\n", buff);
     }
+    printf("Saved %d students", numStudents);
     fclose(fp);
+  }
 }
 
 
@@ -91,30 +93,36 @@ void loadStudents(int key)
   //  - when loading it is best to load data into four strings and then (optionally) decrypt the
   //    strings
   //  - call createStudent to correctly create the students
-  char b1[256];
-  char b2[256];
-  char b3[256];
-  char b4[256];
   if(numStudents>0){
     deleteStudents();
   }
   FILE* fp;
-  fp = fopen("studentdata.txt", "r");
+  fp = fopen(STUFILE, "r");
+  while(1){
   if(fp){
+    char b1[256];
+    char b2[256];
+    char b3[256];
+    char b4[256];
     int match = fscanf(fp, "%s %s %s %s", b1, b2, b3, b4);
-  }
-  if(match == 4){
-    if(key != 0){
-      caesarDecrypt(b1, key);
-      caesarDecrypt(b2, key);
-      caesarDecrypt(b3, key);
-      caesarDecrypt(b4, key);
+    if (match==4){
+      if(key != 0){
+        caesarDecrypt(b1, key);
+        caesarDecrypt(b2, key);
+        caesarDecrypt(b3, key);
+        caesarDecrypt(b4, key);
+      }
+      int age;
+      long id;
+      sscanf(b3, "%d", &age);
+      sscanf(b4, "%ld", &id);
+      createStudent(b1, b2, age, id);
+    } else {
+      break;
     }
-    int age;
-    long id;
-    sscanf(b3, "%d", age);
-    sscanf(b4, "%ld", id);
-    createStudent(b1, b2, age, id);
+  }
+  printf("Loaded %d students", numStudents);
+  fclose(fp);
   }
 }
 
@@ -123,7 +131,7 @@ void printStudent(Student* student)
 {
   printf("  Student: %s %s\n", student->firstName, student->lastName);
   printf("    age: %d\n", student->age);
-  printf("    id: %ld\n", student->id); 
+  printf("    id: %ld\n", student->id);
 }
 
 
